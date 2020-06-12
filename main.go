6a1684d9af5d74ab10ba0fbab66cd83a2940f4cf
Andrew Brainerd <drwb333@gmail.com>
2020-06-11 23:41:51 -0400

    Add POST endpoint

--- main.go
+++ main.go
@@ -1,16 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+type Person struct {
+	name string `json:"url" binding:"required"`
+}
+
 func main() {
 	router := gin.Default()
-	router.GET("/", func(context *gin.Context) {
+
+	router.GET("/api", func(context *gin.Context) {
 		context.JSON(http.StatusOK, gin.H{
 			"message": "Welcome to a Go API",
+		})
+	})
+
+	router.POST("/api/name", func(context *gin.Context) {
+		var person Person
+		fmt.Println(context)
+		context.BindJSON(&person)
+		fmt.Println(person)
+		context.JSON(http.StatusOK, gin.H{
+			"message": fmt.Sprintf("Hello %v", person.name),
 		})
 	})
 
